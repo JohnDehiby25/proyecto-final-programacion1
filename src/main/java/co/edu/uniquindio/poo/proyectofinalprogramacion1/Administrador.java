7ab59356ff9910087e66a8bfb1164f20ce86aff5
JohnDehiby25 <dehibymorales@gmail.com>
2025-04-17 18:07:11 -0400

    Realizacion de la clase administrador
    Se realizo la clase administrador con los metodos abstractos necesarios
    para poder registrar y actualizar datos

---
+++ src/main/java/co/edu/uniquindio/poo/proyectofinalprogramacion1/Administrador.java
@@ -0,0 +1,55 @@
+package co.edu.uniquindio.poo.proyectofinalprogramacion1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Administrador extends Usuario {
+	private ArrayList<Medico> listMedicos;
+	private ArrayList<Paciente> listPacientes;
+	private ArrayList<Sala> listSalas;
+	private ArrayList<CitaMedica> listCitasMedicas;
+	
+	public Administrador(String nombre,String id, String correo, String telefono, int edad) {
+		super(nombre,id,correo,telefono,edad);
+		this.listMedicos=new ArrayList<>();
+		this.listPacientes=new ArrayList<>();
+		this.listSalas = new ArrayList<>();
+		this.listCitasMedicas= new ArrayList<>();
+	}
+	@Override
+	public void registrarDatosPersonales() {
+		System.out.println("\n Registro de datos Administrador");
+		System.out.println("Nombre: " + nombre);
+		System.out.println("Id: " + id);
+		System.out.println("Correo: " + correo);
+		System.out.println("Telefono: " + telefono);
+		System.out.println("Edad: " + edad);
+
+
+		
+	}
+	@Override
+	public void actualizarDatosPersonales() {
+		Scanner actualizacionDatos = new Scanner(System.in);
+		
+		System.out.println("Actualice su nombre:");
+		setNombre(actualizacionDatos.nextLine());
+		
+		System.out.println("Actualice su id:");
+		setId(actualizacionDatos.nextLine());
+		
+		System.out.println("Actualice su correo:");
+		setCorreo(actualizacionDatos.nextLine());
+		
+		System.out.println("Actualice su telefono:");
+		setTelefono(actualizacionDatos.nextLine());
+		
+		System.out.println("Actualice su edad:");
+		setEdad(actualizacionDatos.nextInt());
+		
+		actualizacionDatos.nextLine();
+		System.out.println("Sus datos fueron actualizados correctamente");
+		
+		actualizacionDatos.close();
+	}
+}
