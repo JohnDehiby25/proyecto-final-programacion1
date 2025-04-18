e80977d1e54acb8e2801c382d22579e134f39c39
JohnDehiby25 <dehibymorales@gmail.com>
2025-04-17 21:58:09 -0400

    Documentacion proyecto en clases
    Se realizo la documentacion o de todas las clases al inicio para
    informar de que va a tratar el proyecto con el nombre de los integrantes
    y fechas

--- src/main/java/co/edu/uniquindio/poo/proyectofinalprogramacion1/Administrador.java
+++ src/main/java/co/edu/uniquindio/poo/proyectofinalprogramacion1/Administrador.java
@@ -1,3 +1,14 @@
+/* 
+ * Proyecto final de sistema de gestion de hospital
+ * 
+ * Integrantes
+ * John Dehiby Morales Guzmán
+ * Darwin Castro
+ * Miguel Angel Diaz Rodriguez
+ * 
+ * Version: 1.0 Abril 2025
+ */
+
 package co.edu.uniquindio.poo.proyectofinalprogramacion1;
 
 import java.time.LocalDateTime;
--- src/main/java/co/edu/uniquindio/poo/proyectofinalprogramacion1/Sala.java
+++ src/main/java/co/edu/uniquindio/poo/proyectofinalprogramacion1/Sala.java
@@ -1,3 +1,13 @@
+/* 
+ * Proyecto final de sistema de gestion de hospital
+ * 
+ * Integrantes
+ * John Dehiby Morales Guzmán
+ * Darwin Castro
+ * Miguel Angel Diaz Rodriguez
+ * 
+ * Version: 1.0 Abril 2025
+ */
 package co.edu.uniquindio.poo.proyectofinalprogramacion1;
 
 public class Sala {
